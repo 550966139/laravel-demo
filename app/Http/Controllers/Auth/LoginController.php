480a4dd8570057235a1b4b98970b286a9f305825
less <wenshikin@icloud.com>
2019-07-14 16:43:46 +0800

    action_pull

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 use EasyWeChat\Factory;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -111,5 +112,26 @@ class LoginController extends Controller
         ];
         $res=$app->menu->create($buttons);
         dd($res);
+    }
+
+    public function git_pull(Request $request)
+    {
+        //git webhook 自动部署脚本
+        //项目存放物理路径
+        $path = "/data/wwwroot/laravel-demo";
+        $requestBody = file_get_contents("php://input");
+        if (empty($requestBody)) {
+            return 'send fail';
+        }
+        $content = json_decode($requestBody, true);
+        //若是主分支且提交数大于0
+        if ($content['ref']=='refs/heads/master' && $content['total_commits_count']>0) {
+            $res = shell_exec("cd {$path} && git pull origin master 2>&1");//以www用户运行
+            $res_log = '-------------------------'.PHP_EOL;
+            $res_log .= $content['user_name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . $content['total_commits_count'] . '个commit：' . PHP_EOL;
+            $res_log .= $res.PHP_EOL;
+            file_put_contents("git-webhook.txt", $res_log, FILE_APPEND);//追加写入
+        }
+        echo '很棒:'.date('y-m-d H:i:s');
     }
 }
