e2bb71175563b706b68c007cf6d2116d8fd2ed3f
wenshikun <wenshikin@icloud.com>
2019-07-16 17:51:40 +0800

    static

--- app/Models/RedEnvelopes.php
+++ app/Models/RedEnvelopes.php
@@ -9,7 +9,7 @@ class RedEnvelopes extends Model
  * 求一个数的平方
  * @param $n
  */
-    public function sqr($n)
+    public static function sqr($n)
     {
         return $n*$n;
     }
@@ -18,9 +18,9 @@ class RedEnvelopes extends Model
     * 生产min和max之间的随机数，但是概率不是平均的，从min到max方向概率逐渐加大。
     * 先平方，然后产生一个平方值范围内的随机数，再开方，这样就产生了一种“膨胀”再“收缩”的效果。
     */
-    public function xRandom($bonus_min, $bonus_max)
+    public static function xRandom($bonus_min, $bonus_max)
     {
-        $sqr = intval(sqr($bonus_max-$bonus_min));
+        $sqr = intval(self::sqr($bonus_max-$bonus_min));
         $rand_num = rand(0, ($sqr-1));
         return intval(sqrt($rand_num));
     }
@@ -34,7 +34,7 @@ class RedEnvelopes extends Model
     * @param $bonus_min 每个小红包的最小额
     * @return 存放生成的每个小红包的值的一维数组
     */
-    public function getBonus($bonus_total, $bonus_count, $bonus_max, $bonus_min)
+    public static function getBonus($bonus_total, $bonus_count, $bonus_max, $bonus_min)
     {
         $result = array();
   
@@ -46,8 +46,8 @@ class RedEnvelopes extends Model
         //
         //这样的随机数的概率实际改变了，产生大数的可能性要比产生小数的概率要小。
         //这样就实现了大部分红包的值在平均数附近。大红包和小红包比较少。
-        $range1 = sqr($average - $bonus_min);
-        $range2 = sqr($bonus_max - $average);
+        $range1 = self::sqr($average - $bonus_min);
+        $range2 = self::sqr($bonus_max - $average);
   
         for ($i = 0; $i < $bonus_count; $i++) {
             //因为小红包的数量通常是要比大红包的数量要多的，因为这里的概率要调换过来。
@@ -55,12 +55,12 @@ class RedEnvelopes extends Model
             //当随机数<平均值，则产生大红包
             if (rand($bonus_min, $bonus_max) > $average) {
                 // 在平均线上减钱
-                $temp = $bonus_min + xRandom($bonus_min, $average);
+                $temp = $bonus_min + self::xRandom($bonus_min, $average);
                 $result[$i] = $temp;
                 $bonus_total -= $temp;
             } else {
                 // 在平均线上加钱
-                $temp = $bonus_max - xRandom($average, $bonus_max);
+                $temp = $bonus_max - self::xRandom($average, $bonus_max);
                 $result[$i] = $temp;
                 $bonus_total -= $temp;
             }
